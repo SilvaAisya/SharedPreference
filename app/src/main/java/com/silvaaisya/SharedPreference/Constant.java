3f39e1615879723e4971fe2ccb0d3b3e0c03a129
SilvaAisya <silvakdr68@gmail.com>
2019-04-12 07:23:13 +0700

    SharedPreference

---
+++ app/src/main/java/com/silvaaisya/SharedPreference/Constant.java
@@ -0,0 +1,12 @@
+package com.silvaaisya.SharedPreference;
+
+public class Constant {
+
+    public static final String LAYOUT_MODE = "layout_mode";
+    public static final String SESSION = "session";
+
+    public static final int LAYOUT_MODE_LIST = 0;
+    public static final int LAYOUT_MODE_GRID = 1;
+    public static final String PREF_TEXT_SIZE = "pref_text_size";
+}
+
---
+++ app/src/main/java/com/silvaaisya/SharedPreference/fragments/SettingsFragment.java
@@ -0,0 +1,15 @@
+package com.silvaaisya.SharedPreference.fragments;
+
+import android.os.Bundle;
+import com.silvaaisya.SharedPreference.R;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
+
